6e1023362f7ec9e2737d15efb7453b905045e152
SLISETTE127 <SLISETTE127@GMAIL.COM>
2021-06-13 12:01:03 -0500

    fixed fetch

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const pokemonListCointainer = document.getElementById('pokemon-list');
+const pokemonListContainer = document.getElementById('pokemon-list');
 
 const URL = ` https://pokeapi.co/api/v2/pokemon/`;
 
 
 fetch(URL).then((response) => response.json()).then((data) => {
+  console.log(data.results)
   data.results.forEach((pokemon) => {
+    console.log(pokemon);
   const li = document.createElement('li');
-      li.innerHtml = data.results.name; 
-      pokemonListCointainer.appendChild(li)  
-
+  const textNode = document.createTextNode(pokemon.name);
+      li.appendChild(textNode); 
+      pokemonListContainer.appendChild(li)  
   })  
   
-  // const li = document.createElement('li');
+ 
     
     
 
